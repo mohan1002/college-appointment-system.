6eb05a596df0f7bdd9418ef7328e082f37f4e8dc
mohan1002 <97791635+mohan1002@users.noreply.github.com>
2024-12-18 14:29:43 +0530

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+college-appointment-system/
+├── app/
+│   ├── __init__.py
+│   ├── models.py
+│   ├── routes.py
+│   └── database.db (leave this empty for now)
+├── tests/
+│   └── test_endpoints.py
+├── .env (optional, for secrets)
+├── requirements.txt
+└── run.py
+
